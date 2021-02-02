782111dda346b98c593d69f22fe0b25c14f888ba
ElManu3le <62712661+ElManu3le@users.noreply.github.com>
2021-02-02 15:27:46 +0100

    cosa

--- Practica2/Practica2/src/main/java/Practica/Dao/ClienteDao.java
+++ Practica2/Practica2/src/main/java/Practica/Dao/ClienteDao.java
@@ -1,53 +1,42 @@
 package Practica.Dao;
 
-import Practica.HibernateUtil.HibernateUtil;
-import Practica.ModelClass.*;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.*;
+import org.hibernate.Session;
 
+import Practica.HibernateUtil.HibernateUtil;
 import Practica.ModelClass.Cliente;
 
 public class ClienteDao implements Dao<Cliente> {
 
     Session srf = HibernateUtil.getSession();
 
-   
-    
-   
-    
-
     @Override
     public Optional<Cliente> get(long id) {
-        
+
         return null;
     }
 
     @Override
     public List<Cliente> getAll() {
-        
+
         return null;
     }
 
     @Override
     public void save(Cliente t) throws Exception {
 
-        
-        
-
     }
 
     @Override
     public void update(Cliente t, String[] params) {
-        
 
     }
 
     @Override
     public void delete(Cliente t) {
-        
 
     }
-    
+
 }
